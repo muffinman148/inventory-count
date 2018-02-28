b5fd4ccf70dd0a128edb0b02840590f8807f6b87
Marc Geis <geis006@cougars.csusm.edu>
2018-02-27 17:03:32 -0800

    Flask, Server - Adds new flask web server. Adds small database management.

--- server/invServer.py
+++ server/invServer.py
@@ -1,14 +1,43 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-import socket
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+# Example
+# engine = create_engine('sqlite:///tutorial.db', echo=True)
+
+# mysql-python
+engine = create_engine('mysql+mysqldb://pi:12345@localhost/invLocal')
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" 
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
