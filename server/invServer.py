3e57f0d8405e87dd16f879b5177736f9fb73e384
Marc Geis <geis006@cougars.csusm.edu>
2018-03-27 17:01:42 -0700

    Plugins - Removes SQLAlchemy and assets. Removes form based on SQLAlchemy.

--- server/invServer.py
+++ server/invServer.py
@@ -1,34 +1,23 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-from sqlalchemy.orm import sessionmaker
-from tabledef import *
-# Example
-# engine = create_engine('sqlite:///tutorial.db', echo=True)
-
-# mysql-python
-engine = create_engine('mysql+mysqldb://pi:12345@localhost/invLocal')
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return redirect(url_for('login'))
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!  <a href='/logout'>Logout</a>"
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    POST_USERNAME = str(request.form['username'])
-    POST_PASSWORD = str(request.form['password'])
- 
-    Session = sessionmaker(bind=engine)
-    s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-    result = query.first()
-    if result:
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-    return home()
+    return "This is a login Page"
+
+@app.route("/printlabels")
+def print_label():
+    return render_template('printlabels.html')
 
 @app.route("/logout")
 def logout():
@@ -40,5 +29,7 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
 
